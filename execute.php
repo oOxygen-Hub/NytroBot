7a0f1787f92058c41a8d8820ed20121c9b93a7e2
oOxygen-Hub <58358660+oOxygen-Hub@users.noreply.github.com>
2019-11-30 13:09:46 +0100

    Add files via upload

--- execute.php
+++ execute.php
@@ -1,12 +1,10 @@
 <?php
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
-
 if(!$update)
 {
   exit;
 }
-
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
@@ -15,11 +13,26 @@ $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name']
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
-
 $text = trim($text);
 $text = strtolower($text);
-
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
+$response = '';
+if(strpos($text, "/start") === 0 || $text=="ciao")
+{
+	$response = "Ciao $firstname, benvenuto!";
+}
+elseif($text=="name")
+{
+	$response = "Im NytroBot, created by oOxygen";
+}
+elseif($text=="domanda 2")
+{
+	$response = "risposta 2";
+}
+else
+{
+	$response = "Comando non valido!";
+}
+$parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
