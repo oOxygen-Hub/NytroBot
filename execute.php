c41e1dd123848312e2516c4ff09ba30a0da647f0
oOxygen-Hub <58358660+oOxygen-Hub@users.noreply.github.com>
2019-11-30 15:19:17 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,10 +1,14 @@
 <?php
+// recupero il contenuto inviato da Telegram
 $content = file_get_contents("php://input");
+// converto il contenuto da JSON ad array PHP
 $update = json_decode($content, true);
+// se la richiesta è null interrompo lo script
 if(!$update)
 {
   exit;
 }
+// assegno alle seguenti variabili il contenuto ricevuto da Telegram
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
@@ -13,26 +17,20 @@ $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name']
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
+// pulisco il messaggio ricevuto togliendo eventuali spazi prima e dopo il testo
 $text = trim($text);
+// converto tutti i caratteri alfanumerici del messaggio in minuscolo
 $text = strtolower($text);
+// mi preparo a restitutire al chiamante la mia risposta che è un oggetto JSON
+// imposto l'header della risposta
 header("Content-Type: application/json");
-$response = '';
-if(strpos($text, "/start") === 0 || $text=="ciao")
-{
-	$response = "Ciao $firstname, benvenuto!";
-}
-elseif($text=="ciao")
-{
-	$response = "Ciao, benvenuto $firstname";
-}
-elseif($text=="oxy")
-{
-	$response = "oOxygen è il mio Creatore";
-}
-else
-{
-	$response = "Scusa non ho Capito!";
-}
-$parameters = array('chat_id' => $chatId, "text" => $response);
+// la mia risposta è un array JSON composto da chat_id, text, method
+// chat_id mi consente di rispondere allo specifico utente che ha scritto al bot
+// text è il testo della risposta
+$parameters = array('chat_id' => $chatId, "text" => $text);
+// method è il metodo per l'invio di un messaggio (cfr. API di Telegram)
 $parameters["method"] = "sendMessage";
+// imposto la keyboard
+$parameters["reply_markup"] = '{ "keyboard": [["uno"], ["due"], ["tre"], ["quattro"]], "one_time_keyboard": false}';
+// converto e stampo l'array JSON sulla response
 echo json_encode($parameters);
