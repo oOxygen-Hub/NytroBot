750a75272e168e0de95afab62a23b46cf49fff59
oOxygen-Hub <58358660+oOxygen-Hub@users.noreply.github.com>
2019-11-30 15:25:13 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,14 +1,10 @@
 <?php
-// recupero il contenuto inviato da Telegram
 $content = file_get_contents("php://input");
-// converto il contenuto da JSON ad array PHP
 $update = json_decode($content, true);
-// se la richiesta è null interrompo lo script
 if(!$update)
 {
   exit;
 }
-// assegno alle seguenti variabili il contenuto ricevuto da Telegram
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
@@ -16,21 +12,52 @@ $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_nam
 $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
-$text = isset($message['text']) ? $message['text'] : "";
-// pulisco il messaggio ricevuto togliendo eventuali spazi prima e dopo il testo
-$text = trim($text);
-// converto tutti i caratteri alfanumerici del messaggio in minuscolo
-$text = strtolower($text);
-// mi preparo a restitutire al chiamante la mia risposta che è un oggetto JSON
-// imposto l'header della risposta
+$response = '';
+if(isset($message['text']))
+{
+	$response = "Ho ricevuto il seguente messaggio di testo: " . $message['text'];
+}
+elseif(isset($message['audio']))
+{
+	$response = "Ho ricevuto un messaggio audio";
+}
+elseif(isset($message['document']))
+{
+	$response = "Ho ricevuto un messaggio document";
+}
+elseif(isset($message['photo']))
+{
+	$response = "Ho ricevuto un messaggio photo";
+}
+elseif(isset($message['sticker']))
+{
+	$response = "Ho ricevuto un messaggio sticker";
+}
+elseif(isset($message['video']))
+{
+	$response = "Ho ricevuto un messaggio video";
+}
+elseif(isset($message['voice']))
+{
+	$response = "Ho ricevuto un messaggio voice";
+}
+elseif(isset($message['contact']))
+{
+	$response = "Ho ricevuto un messaggio contact";
+}
+elseif(isset($message['location']))
+{
+	$response = "Ho ricevuto un messaggio location";
+}
+elseif(isset($message['venue']))
+{
+	$response = "Ho ricevuto un messaggio venue";
+}
+else
+{
+	$response = "Ho ricevuto un messaggio ?";
+}
 header("Content-Type: application/json");
-// la mia risposta è un array JSON composto da chat_id, text, method
-// chat_id mi consente di rispondere allo specifico utente che ha scritto al bot
-// text è il testo della risposta
-$parameters = array('chat_id' => $chatId, "text" => $text);
-// method è il metodo per l'invio di un messaggio (cfr. API di Telegram)
+$parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
-// imposto la keyboard
-$parameters["reply_markup"] = '{ "keyboard": [["uno"], ["due"], ["tre"], ["quattro"]], "one_time_keyboard": false}';
-// converto e stampo l'array JSON sulla response
 echo json_encode($parameters);
