0f1cd1de212b896c0a8f3294e2f0049d41cdd9c8
oOxygen-Hub <58358660+oOxygen-Hub@users.noreply.github.com>
2019-12-01 05:08:54 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,122 +1,116 @@
 <?php
-	
-	include "config.php"; // relative url to code that contains credential to access to our database
-
-	$botToken = "API_TOKEN"; // Api TOKEN to our bot
-	$website = "https://api.telegram.org/bot".$botToken;
-
-	$FilejSon = file_get_contents("php://input"); // Take the url input, in this case will be executed method getUpdates that return Update.
-	$FilejSon = json_decode($FilejSon, TRUE); // Decode the variable before because now we can search with key (because it's a dictionary)
-
-	$FirstName = $FilejSon["message"]["chat"]["first_name"]; // Get the name that user set
-	$UserChatId = $FilejSon["message"]["chat"]["id"]; // get the User ID, this is unique
-	$Message = $FilejSon["message"]["text"]; // Get the message sent from user
-
-	// Variable only for Inline Keyboard
-	$CallBackID = $FilejSon["callback_query"]["from"]["id"];
-	$CallBackData = $FilejSon["callback_query"]["data"];
-
-	/*
-
-		This Tutorial use 2 Table:
-
-		'TutoriaKeyboard' has these rows:
-			UniqueID -> Integer value and this is the ID of Keyboard
-			NomeLeyout -> The Layout Name (used to identiry the keyboard)
-			CodiceTastiera -> The Keyboard's code ( e.g. &reply_markup={"keyboard":[[{"text":"This is an example Keyboard Code saved in DB"}]]} )
-			Type -> Not useful in this Tutorial (useful later) identify the Keyboard Type (e.g. inline_keboard, keyboard, ...)
-
-		'TutorialCMD' has these rows:
-			ID -> Integer value and this is the ID of command
-			NomeCMD -> The command and how this will be called back (e.g. saved Hello, when user send 'Hello' bot take the reply from this DB)
-			Messaggio -> Message that bot send when 'NomeCMD' was identified
-			LayoutTastiera -> The Keyboard that you wanna show with message (the same of 'NomeLeyout' in 'TutoriaKeyboard' DB)
-			TypeKey -> Not useful in this Tutorial (useful later) identify the Keyboard Type (e.g. inline_keboard, keyboard, ...)
-
-	*/
-
-	if ($UserChatId == 23326587) // Specific chat_id to insert automatically specific command in our DataBase
-	{
-		if (substr_count($Message, "-") == 2) // In our case we need to insert 3 elements so there are only 2 ' - ' 
-		{ 
-			$NewMessage = explode("-", $Message); 
-			// pattern: Array[0] = NAme Comand, Array[1] = What bot Reply, Array[2] = Name Layout Keyboard
-
-			$Insert = mysql_query("INSERT INTO `TutorialCMD` (`ID`, `NomeCMD`, `Messaggio`, `LayoutTastiera`) VALUES (NULL, '$NewMessage[0]', '$NewMessage[1]', '$NewMessage[2]')");
-			$msg = "Name Command: ".$NewMessage[0]." \nWhat bot Reply: ".$NewMessage[1]." \nName Keyboard: ".$NewMessage[2];
-			sendMessage($UserChatId, $msg, "");
-		}
-	}
-
-	switch ($Message)
-	{
-		case '/start':
-			$msg = "Welcome $GLOBALS[FirstName]! I'm a Tutorial Bot.";
-			sendMessage($UserChatId, $msg, "help");
-			break;
-
-		case "chatid":
-			$msg = $UserChatId;
-			sendMessage($UserChatId, $msg, "help");
-			break;
-
-		default:
-
-			if (callback($FilejSon)) // check if input is a callback
-			{
-				if ($CallBackData == "Click") // Check if inline button data has that value
-				{
-					sendMessage($CallBackID, "Ottimo! Sei stato bravo!", "");
-				}
-			}
-
-			else
-			{
-				$Search = mysql_query("SELECT * FROM `TutorialCMD` WHERE `NomeCMD` LIKE '%$Message%'");
-				while ($Row = mysql_fetch_assoc($Search)) 
-				{
-					$Text = $Row["Messaggio"]; // What this bot reply to user
-					$CMD = $Row["NomeCMD"]; // not useful, this will never be used...
-					$ID = $Row["ID"]; // Like CMD, this is not useful so we can delete this code.
-					$NameLayoutKeyboard = $Row["LayoutTastiera"]; // Here we take the name of Keyboard (this will be used in another table of DataBase)
-				}
-				sendMessage($UserChatId, $Text, $NameLayoutKeyboard);
-			}
-
-			break;
-
-	}
-	
-
-	function sendMessage($chat_id, $text, $LayoutKey) // Function that send a message
-	{
-
-		if ($LayoutKey == "") // If LayoutKey is invalid this code will be execute
-		{
-			$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text);
-			file_get_contents($url); // Execute that URL
-		}
-
-		else
-		{
-			$Search2 = mysql_query("SELECT * FROM `TutoriaKeyboard` WHERE `NomeLeyout` LIKE '%$LayoutKey%'"); // Takes the name of Keyboard and takes the jSon code from a specific Table of DataBase that contains jSon codes.
-			while ($Row = mysql_fetch_assoc($Search2)) 
-			{
-				$jSonCodeKeyboard = $Row["CodiceTastiera"]; // this takes a jSon code from our Table.
-			}
-
-			$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
-			file_get_contents($url);
-		}
-	}
-
-	// Function that try to access to callback, if it can be done, will be returned correct values else will be returned false
-	function callback($getUpdate)
-	{
-		if ($getUpdate["callback_query"]) // is possibile to access to 'callback_query' key? If true return true, else return false
-			return true;
-		else
-			return false;
-	}
-
+  include 'token.php';
+  $update = file_get_contents('php://input');
+  $update = json_decode($update, TRUE);
+  $chatId = $update['message']['from']['id'];
+  $nome = $update['message']['from']['first_name'];
+  $text = $update['message']['text'];
+  $query = $update['callback_query'];
+  $queryid = $query['id'];
+  $queryUserId = $query['from']['id'];
+  $queryusername = $query['from']['username'];
+  $querydata = $query['data'];
+  $querymsgid = $query['message']['message_id'];
+  $inlinequery = $update['inline_query'];
+  $inlineid = $inlinequery['id'];
+  $inlineUserId = $inlinequery['from']['id'];
+  $inlinequerydata = $inlinequery['query'];
+  if(isset($update['inline_query'])) {
+    gestisciInlineQuery($inlineid,$inlineUserId,$inlinequerydata,$inlinequery['from']['username'],$inlinequery['from']['first_name'],$inlinequery['from']['last_name']);
+    exit();
+  }
+  $agg = json_encode($update,JSON_PRETTY_PRINT);
+  if($querydata == "StampaMessaggio"){
+    answerQuery($queryid,"Ciao $queryusername! Come stai?!");
+    exit();
+  }
+  if($querydata == "ModificaMessaggio"){
+    editMessageText($queryUserId,$querymsgid,"HEYLA!");
+    exit();
+  }
+   if(strpos($text,"+")!==false){
+          sendMessage($chatId,eval('return '.$text.';'));
+         exit();
+   }
+   $esempiotastierainline = '[{"text":"Testo","url":"http://yt.alexgaming.me"},{"text":"Inline","switch_inline_query":"Ciao!"}],[{"text":"Testo","callback_data":"StampaMessaggio"},{"text":"Modifica Messaggio","callback_data":"ModificaMessaggio"}]';
+  switch($text){
+    case "/start":
+        sendMessage($chatId,"Weyla!");
+        break;
+    case "/tastiera":
+        sendMessage($chatId,"Test tastiera Inline!",$esempiotastierainline,"inline");
+        break;
+    case "Bene":
+        sendMessage($chatId,"Ottimo!");
+        break;
+    case "Tu?":
+        sendMessage($chatId,"Eh... Sono ancora in via di sviluppo!");
+        break;
+    default:
+      $tastierabenvenuto = '["Bene"],["Tu?"],["'.$nome.'"]';
+      sendMessage($chatId,"Ciao <b>$nome</b>! Come stai?",$tastierabenvenuto,"fisica");
+      break;
+  }
+  function sendMessage($chatId,$text,$tastiera,$tipo){
+    if(isset($tastiera)){
+      if($tipo == "fisica"){
+        $tastierino = '&reply_markup={"keyboard":['.urlencode($tastiera).'],"resize_keyboard":true}';
+      }
+      else {
+        $tastierino = '&reply_markup={"inline_keyboard":['.urlencode($tastiera).'],"resize_keyboard":true}';
+      }
+    }
+    $url = $GLOBALS[website]."/sendMessage?chat_id=$chatId&parse_mode=HTML&text=".urlencode($text).$tastierino;
+    file_get_contents($url);
+  }
+  function answerQuery($callback_query_id,$text){
+    $url = $GLOBALS[website]."/answerCallbackQuery?callback_query_id=$callback_query_id&text=".urlencode($text);
+    file_get_contents($url);
+  }
+  function editMessageText($chatId,$message_id,$newText)
+  {
+    $url = $GLOBALS[website]."/editMessageText?chat_id=$chatId&message_id=$message_id&parse_mode=HTML&text=".urlencode($newText);
+    file_get_contents($url);
+  }
+  function gestisciInlineQuery($queryId,$chatId,$querydata,$username,$name,$cognome)
+  {
+       if(strpos($querydata,"+") !== false)
+        {
+              $numeric = eval('return '.$querydata.';');
+              $risultati=[[
+          "type" => "article",
+          "id" => "0",
+          "title" => "Risultato".urlencode($querydata),
+          "input_message_content" => array("message_text" => "<b>".urlencode($querydata)."</b>\n\nRisultato: $numeric", "parse_mode" => "HTML"),
+          "description" => "Descrizione del result",
+          ]
+      ];
+      $risultati = json_encode($risultati,true);
+      $url = $GLOBALS[website]."/answerInlineQuery?inline_query_id=$queryId&results=$risultati&cache_time=0&switch_pm_text=Vai al Bot&switch_pm_parameter=123";
+      file_get_contents($url);
+      exit();
+         }
+      $infoUtente = "<b>Ciao! Io sono $username</b>\n\nIl mio chatId è <code>$chatId</code>\nIl mio nome è $name\nIl mio cognome è $cognome";
+      $risultati=[[
+          "type" => "article",
+          "id" => "0",
+          "title" => "Titolo del Result",
+          "input_message_content" => array("message_text" => "Testo del Result", "parse_mode" => "HTML"),
+          "reply_markup" => array("inline_keyboard" => [[array("text" => "CLICCA QUI","url" => "yt.alexgaming.me")],[array("text" => "CLICCA QUI","callback_data" => "StampaMessaggio")]]),
+          "description" => "Descrizione del result",
+          ],
+          [
+              "type" => "article",
+              "id" => "1",
+              "title" => "Invia le tue informazioni",
+              "input_message_content" => array("message_text" => "$infoUtente", "parse_mode" => "HTML"),
+              "reply_markup" => array("inline_keyboard" => [[array("text" => "CLICCA QUI","url" => "yt.alexgaming.me")],[array("text" => "CLICCA QUI","callback_data" => "StampaMessaggio")]]),
+              "description" => "Descrizione del result",
+              ],
+      ];
+      $risultati = json_encode($risultati,true);
+      $url = $GLOBALS[website]."/answerInlineQuery?inline_query_id=$queryId&results=$risultati&cache_time=0&switch_pm_text=Vai al Bot&switch_pm_parameter=123";
+      file_get_contents($url);
+      exit();
+  }
 ?>
