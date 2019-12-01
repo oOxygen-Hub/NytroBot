29938342e8678731c70e4fea929988c2517da303
oOxygen-Hub <58358660+oOxygen-Hub@users.noreply.github.com>
2019-12-01 09:51:54 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -43,7 +43,7 @@
         sendMessage($chatId,"Ciao <b>$nome</b>! Come posso esserti utile?",$tastierabenvenuto,"fisica");
         break;
     case "Official Channel":
-	    $email_body = "Questi sono i dettagli:<ul><li><strong>Nome:</b> '.$nome.'</li><li><strong>Cognome:</b>"; 
+	    $email_body = "Questi sono i dettagli"; 
         sendMessage($chatId,$email_body,$tastierabenvenuto,"fisica");
         break;
     case "Menu":
