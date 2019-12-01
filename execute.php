7022d5b6658eadb7592957b4093d726440201168
oOxygen-Hub <58358660+oOxygen-Hub@users.noreply.github.com>
2019-12-01 06:22:55 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -45,8 +45,8 @@
         sendMessage($chatId,"Test tastiera Inline!",$esempiotastierainline,"inline");
         break;
     case "Links":
-        sendMessage($chatId,"http://ooxygen.tech");
-        break;
+        $tastierabenvenuto = '["Menu","Links"],["'.$nome.'"]';
+        sendMessage($chatId,"Ciao <b>$nome</b>! uffaaaaaaaaaaaa",$tastierabenvenuto,"fisica");
     default:
       //$tastierabenvenuto = '["Bene"],["Tu?"],["'.$nome.'"]';
       //sendMessage($chatId,"Ciao <b>$nome</b>! Come stai?",$tastierabenvenuto,"fisica");
@@ -75,45 +75,12 @@
   }
   function gestisciInlineQuery($queryId,$chatId,$querydata,$username,$name,$cognome)
   {
-       if(strpos($querydata,"+") !== false)
-        {
-              $numeric = eval('return '.$querydata.';');
-              $risultati=[[
-          "type" => "article",
-          "id" => "0",
-          "title" => "Risultato".urlencode($querydata),
-          "input_message_content" => array("message_text" => "<b>".urlencode($querydata)."</b>\n\nRisultato: $numeric", "parse_mode" => "HTML"),
-          "description" => "Descrizione del result",
-          ]
-      ];
-      $risultati = json_encode($risultati,true);
-      $url = $GLOBALS[website]."/answerInlineQuery?inline_query_id=$queryId&results=$risultati&cache_time=0&switch_pm_text=Vai al Bot&switch_pm_parameter=123";
-      file_get_contents($url);
-      exit();
-         }
-      $infoUtente = "<b>Ciao! Io sono $username</b>\n\nIl mio chatId è <code>$chatId</code>\nIl mio nome è $name\nIl mio cognome è $cognome";
-      $risultati=[[
-          "type" => "article",
-          "id" => "0",
-          "title" => "Titolo del Result",
-          "input_message_content" => array("message_text" => "Testo del Result", "parse_mode" => "HTML"),
-          "reply_markup" => array("inline_keyboard" => [[array("text" => "CLICCA QUI","url" => "yt.alexgaming.me")],[array("text" => "CLICCA QUI","callback_data" => "StampaMessaggio")]]),
-          "description" => "Descrizione del result",
-          ],
-          [
-              "type" => "article",
-              "id" => "1",
-              "title" => "Invia le tue informazioni",
-              "input_message_content" => array("message_text" => "$infoUtente", "parse_mode" => "HTML"),
-              "reply_markup" => array("inline_keyboard" => [[array("text" => "CLICCA QUI","url" => "yt.alexgaming.me")],[array("text" => "CLICCA QUI","callback_data" => "StampaMessaggio")]]),
-              "description" => "Descrizione del result",
-              ],
-      ];
       $risultati = json_encode($risultati,true);
       $url = $GLOBALS[website]."/answerInlineQuery?inline_query_id=$queryId&results=$risultati&cache_time=0&switch_pm_text=Vai al Bot&switch_pm_parameter=123";
       file_get_contents($url);
       exit();
   }
+
 ?>
 
 
