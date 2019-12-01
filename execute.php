2b2dffb05b0ae04e1bd969630c8fe8be86187f10
oOxygen-Hub <58358660+oOxygen-Hub@users.noreply.github.com>
2019-12-01 07:09:22 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,36 +1,81 @@
 <?php
-$content = file_get_contents("php://input");
-$update = json_decode($content, true);
-if(!$update)
-{
-  exit;
-}
-$message = isset($update['message']) ? $update['message'] : "";
-$messageId = isset($message['message_id']) ? $message['message_id'] : "";
-$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
-$date = isset($message['date']) ? $message['date'] : "";
-$text = isset($message['text']) ? $message['text'] : "";
-$text = trim($text);
-$text = strtolower($text);
-header("Content-Type: application/json");
-$response = '';
-if(strpos($text, "/start") === 0 || $text=="ciao")
-{
-	$response = "Ciao, $username benvenuto!";
-}
-elseif($text=="ciao")
-{
-	$response = "Ciao, benvenuto $username";
-}
-elseif($text=="oxy")
-{
-	$response = "oOxygen è il mio Creatore cosa vuoi da lui?";
+  include 'token.php';
+  $update = file_get_contents('php://input');
+  $update = json_decode($update, TRUE);
+  $chatId = $update['message']['from']['id'];
+  $nome = $update['message']['from']['first_name'];
+  $text = $update['message']['text'];
+  $query = $update['callback_query'];
+  $queryid = $query['id'];
+  $queryUserId = $query['from']['id'];
+  $queryusername = $query['from']['username'];
+  $querydata = $query['data'];
+  $querymsgid = $query['message']['message_id'];
+  $inlinequery = $update['inline_query'];
+  $inlineid = $inlinequery['id'];
+  $inlineUserId = $inlinequery['from']['id'];
+  $inlinequerydata = $inlinequery['query'];
+  if(isset($update['inline_query'])) {
+    gestisciInlineQuery($inlineid,$inlineUserId,$inlinequerydata,$inlinequery['from']['username'],$inlinequery['from']['first_name'],$inlinequery['from']['last_name']);
+    exit();
+  }
+  $agg = json_encode($update,JSON_PRETTY_PRINT);
+  if($querydata == "StampaMessaggio"){
+    answerQuery($queryid,"Ciao $queryusername! Come stai?!");
+    exit();
+  }
+  if($querydata == "ModificaMessaggio"){
+    editMessageText($queryUserId,$querymsgid,"HEYLA!");
+    exit();
+  }
+   if(strpos($text,"+")!==false){
+          sendMessage($chatId,eval('return '.$text.';'));
+         exit();
+   }
+   $esempiotastierainline = '[{"text":"Forum","url":"http://ooxygen.tech"},{"text":"Inline","switch_inline_query":"Ciao!"}],[{"text":"Testo","callback_data":"StampaMessaggio"},{"text":"Modifica Messaggio","callback_data":"ModificaMessaggio"}]';
+  
+  switch($text){
+    case "/start":
+        $tastierabenvenuto = '["Menu","Links"],["'.$nome.'"]';
+        sendMessage($chatId,"Ciao <b>$nome</b>! Come posso esserti utile?",$tastierabenvenuto,"fisica");
+        break;
+    case "/tastiera":
+        sendMessage($chatId,"Test tastiera Inline!",$esempiotastierainline,"inline");
+        break;
+    case "Menu":
+        sendMessage($chatId,"Test tastiera Inline!",$esempiotastierainline,"inline");
+        break;
+    case "Links":
+        sendMessage($chatId,"Ottimo!");
+    default:
+      //$tastierabenvenuto = '["Bene"],["Tu?"],["'.$nome.'"]';
+      //sendMessage($chatId,"Ciao <b>$nome</b>! Come stai?",$tastierabenvenuto,"fisica");
+      break;
+  }
+  
+  function sendMessage($chatId,$text,$tastiera,$tipo){
+    if(isset($tastiera)){
+      if($tipo == "fisica"){
+        $tastierino = '&reply_markup={"keyboard":['.urlencode($tastiera).'],"resize_keyboard":true}';
+      }
+      else {
+        $tastierino = '&reply_markup={"inline_keyboard":['.urlencode($tastiera).'],"resize_keyboard":true}';
+      }
+    }
+    $url = $GLOBALS[website]."/sendMessage?chat_id=$chatId&parse_mode=HTML&text=".urlencode($text).$tastierino;
+    file_get_contents($url);
+  }
+  
+  function answerQuery($callback_query_id,$text){
+    $url = $GLOBALS[website]."/answerCallbackQuery?callback_query_id=$callback_query_id&text=".urlencode($text);
+    file_get_contents($url);
+  }
 
-}
-$parameters = array('chat_id' => $chatId, "text" => $response);
-$parameters["method"] = "sendMessage";
-echo json_encode($parameters);
+  function gestisciInlineQuery($queryId,$chatId,$querydata,$username,$name,$cognome)
+  {
+      $risultati = json_encode($risultati,true);
+      $url = $GLOBALS[website]."/answerInlineQuery?inline_query_id=$queryId&results=$risultati&cache_time=0&switch_pm_text=Vai al Bot&switch_pm_parameter=123";
+      file_get_contents($url);
+      exit();
+  }
 
