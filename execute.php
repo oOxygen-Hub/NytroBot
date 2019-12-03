2cb0e36d742b239ca19ff689cc12f526e81983a0
oOxygen-Hub <58358660+oOxygen-Hub@users.noreply.github.com>
2019-12-03 22:45:42 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,120 +1,77 @@
 <?php
 	
-	include "config.php"; // relative url to code that contains credential to access to our database
-
-	$botToken = "API_TOKEN"; // Api TOKEN to our bot
+	$botToken = "1036235226:AAEHiMaeIW0CRa34AvVG7H4JWuUBJn_poIM"; // Api TOKEN to our bot
 	$website = "https://api.telegram.org/bot".$botToken;
 
 	$FilejSon = file_get_contents("php://input"); // Take the url input, in this case will be executed method getUpdates that return Update.
 	$FilejSon = json_decode($FilejSon, TRUE); // Decode the variable before because now we can search with key (because it's a dictionary)
 
 	$FirstName = $FilejSon["message"]["chat"]["first_name"]; // Get the name that user set
-	$UserChatId = $FilejSon["message"]["chat"]["id"]; // get the User ID, this is unique
+	$ChatID = $FilejSon["message"]["chat"]["id"]; // get the User ID, this is unique
 	$Message = $FilejSon["message"]["text"]; // Get the message sent from user
-
-	// Variable only for Inline Keyboard
-	$CallBackID = $FilejSon["callback_query"]["from"]["id"];
-	$CallBackData = $FilejSon["callback_query"]["data"];
-
-	/*
-
-		This Tutorial use 2 Table:
-
-		'TutoriaKeyboard' has these rows:
-			UniqueID -> Integer value and this is the ID of Keyboard
-			NomeLeyout -> The Layout Name (used to identiry the keyboard)
-			CodiceTastiera -> The Keyboard's code ( e.g. &reply_markup={"keyboard":[[{"text":"This is an example Keyboard Code saved in DB"}]]} )
-			Type -> Not useful in this Tutorial (useful later) identify the Keyboard Type (e.g. inline_keboard, keyboard, ...)
-
-		'TutorialCMD' has these rows:
-			ID -> Integer value and this is the ID of command
-			NomeCMD -> The command and how this will be called back (e.g. saved Hello, when user send 'Hello' bot take the reply from this DB)
-			Messaggio -> Message that bot send when 'NomeCMD' was identified
-			LayoutTastiera -> The Keyboard that you wanna show with message (the same of 'NomeLeyout' in 'TutoriaKeyboard' DB)
-			TypeKey -> Not useful in this Tutorial (useful later) identify the Keyboard Type (e.g. inline_keboard, keyboard, ...)
-
-	*/
-
-	if ($UserChatId == 23326587) // Specific chat_id to insert automatically specific command in our DataBase
-	{
-		if (substr_count($Message, "-") == 2) // In our case we need to insert 3 elements so there are only 2 ' - ' 
-		{ 
-			$NewMessage = explode("-", $Message); 
-			// pattern: Array[0] = NAme Comand, Array[1] = What bot Reply, Array[2] = Name Layout Keyboard
-
-			$Insert = mysql_query("INSERT INTO `TutorialCMD` (`ID`, `NomeCMD`, `Messaggio`, `LayoutTastiera`) VALUES (NULL, '$NewMessage[0]', '$NewMessage[1]', '$NewMessage[2]')");
-			$msg = "Name Command: ".$NewMessage[0]." \nWhat bot Reply: ".$NewMessage[1]." \nName Keyboard: ".$NewMessage[2];
-			sendMessage($UserChatId, $msg, "");
-		}
-	}
 
 	switch ($Message)
 	{
 		case '/start':
-			$msg = "Welcome $GLOBALS[FirstName]! I'm a Tutorial Bot.";
-			sendMessage($UserChatId, $msg, "help");
+			$msg = "Welcome $FirstName! I'm a Tutorial Bot.";
+			showKeyboard($ChatID, $msg);
+			break;
+
+		case '/keyboard': // Command to show normal Keyboard
+			$msg = "This is a Tutorial, this Keyboard has 3 buttons, click one to test.";
+			showKeyboard($ChatID, $msg);
 			break;
 
 		case "chatid":
-			$msg = $UserChatId;
-			sendMessage($UserChatId, $msg, "help");
+			$msg = $ChatID;
+			sendMessage($ChatID, $msg);
+			break;
+
+		case "Normal Keyboard": // This is the same text inside a Keyboard
+			$msg = "Abracadabra and keyboard will appear!";
+			showKeyboard($ChatID, $msg);
+			break;
+
+		case "Inline Keyboard": // This is the same text inside a Keyboard
+			$msg = "Abracadabra and inline keyboard will appear!";
+			inlineKeyboard($ChatID, $msg);
+			break;
+
+		case "Remove Keyboard": // This is the same text inside a Keyboard
+			$msg = "Abracadabra and keyboard will disappear!";
+			removeKeyboard($ChatID, $msg);
 			break;
 
 		default:
-
-			if (callback($FilejSon)) // check if input is a callback
-			{
-				if ($CallBackData == "Click") // Check if inline button data has that value
-				{
-					sendMessage($CallBackID, "Ottimo! Sei stato bravo!", "");
-				}
-			}
-
-			else
-			{
-				$Search = mysql_query("SELECT * FROM `TutorialCMD` WHERE `NomeCMD` LIKE '%$Message%'");
-				while ($Row = mysql_fetch_assoc($Search)) 
-				{
-					$Text = $Row["Messaggio"]; // What this bot reply to user
-					$CMD = $Row["NomeCMD"]; // not useful, this will never be used...
-					$ID = $Row["ID"]; // Like CMD, this is not useful so we can delete this code.
-					$NameLayoutKeyboard = $Row["LayoutTastiera"]; // Here we take the name of Keyboard (this will be used in another table of DataBase)
-				}
-				sendMessage($UserChatId, $Text, $NameLayoutKeyboard);
-			}
-
+			$msg = "Unknown Command! So sorry ;(";
+			sendMessage($ChatId, $msg);
 			break;
-
-	}
+	} 
 	
 
-	function sendMessage($chat_id, $text, $LayoutKey) // Function that send a message
+	function sendMessage($chat_id, $text)
 	{
-
-		if ($LayoutKey == "") // If LayoutKey is invalid this code will be execute
-		{
-			$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text);
-			file_get_contents($url); // Execute that URL
-		}
-
-		else
-		{
-			$Search2 = mysql_query("SELECT * FROM `TutoriaKeyboard` WHERE `NomeLeyout` LIKE '%$LayoutKey%'"); // Takes the name of Keyboard and takes the jSon code from a specific Table of DataBase that contains jSon codes.
-			while ($Row = mysql_fetch_assoc($Search2)) 
-			{
-				$jSonCodeKeyboard = $Row["CodiceTastiera"]; // this takes a jSon code from our Table.
-			}
-
-			$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
-			file_get_contents($url);
-		}
+		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text);
+		file_get_contents($url);
 	}
 
-	// Function that try to access to callback, if it can be done, will be returned correct values else will be returned false
-	function callback($getUpdate)
+	function showKeyboard($chat_id, $text)
 	{
-		if ($getUpdate["callback_query"]) // is possibile to access to 'callback_query' key? If true return true, else return false
-			return true;
-		else
-			return false;
+		$jSonCodeKeyboard = '&reply_markup={"keyboard":[["Normal%20Keyboard"],["Hide%20Keyboard","Remove%20Keyboard"]],"one_time_keyboard":true}';
+		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
+		file_get_contents($url);
+	}
+
+	function removeKeyboard($chat_id, $text)
+	{
+		$jSonCodeKeyboard = '&reply_markup={"remove_keyboard":true}';
+		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
+		file_get_contents($url);
+	}
+
+	function inlineKeyboard($chat_id, $text) // This is an useless type of this keyboard, in a specific Tutorial I show an useful usage of this keyboard.
+	{
+		$jSonCodeKeyboard = '&reply_markup={"inline_keyboard":[[{"text":"API%20Bot%20Telegram","url":"https://core.telegram.org/bots/api"},{"text":"Google","url":"https://www.google.com"}]]}';
+		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
+		file_get_contents($url);
 	}
