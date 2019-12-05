72a620973180408bf3d0cecd7bd9dbcf43dcdfe9
oOxygen-Hub <58358660+oOxygen-Hub@users.noreply.github.com>
2019-12-05 10:15:54 +0100

    Update nytrobot.php

--- nytrobot.php
+++ nytrobot.php
@@ -63,8 +63,8 @@
 
 	function showKeyboard($chat_id, $text)
 	{
-		//$jSonCodeKeyboard = '&reply_markup={"keyboard":[["Normal%20Keyboard"],["Hide%20Keyboard","Remove%20Keyboard"]],"resize_keyboard":true}';
-		$jSonCodeKeyboard = '&reply_markup={"keyboard":["text":"Prova","callback_data":"Remove Keyboard"]}';
+		$jSonCodeKeyboard = '&reply_markup={"keyboard":[["Normal%20Keyboard"],["Hide%20Keyboard","Remove%20Keyboard"]],"resize_keyboard":true}';
+		//$jSonCodeKeyboard = '&reply_markup={"keyboard":["text":"Prova","callback_data":"Remove Keyboard"]}';
 		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
 		file_get_contents($url);
 	}
@@ -82,4 +82,3 @@
 		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
 		file_get_contents($url);
 	}
-
