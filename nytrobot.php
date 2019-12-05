5832b25f10b2cb07b5c2fd09ec90813d49141139
oOxygen-Hub <58358660+oOxygen-Hub@users.noreply.github.com>
2019-12-05 14:29:37 +0100

    Update nytrobot.php

--- nytrobot.php
+++ nytrobot.php
@@ -22,6 +22,30 @@
 			$msg = "This is a Tutorial, this Keyboard has 3 buttons, click one to test.";
 			showKeyboard($ChatID, $msg);
 			break;
+<?php
+	
+	$botToken = "1036235226:AAEHiMaeIW0CRa34AvVG7H4JWuUBJn_poIM"; // Api TOKEN to our bot
+	$website = "https://api.telegram.org/bot".$botToken;
+
+	$FilejSon = file_get_contents("php://input"); // Take the url input, in this case will be executed method getUpdates that return Update.
+	$FilejSon = json_decode($FilejSon, TRUE); // Decode the variable before because now we can search with key (because it's a dictionary)
+
+	$FirstName = $FilejSon["message"]["chat"]["first_name"]; // Get the name that user set
+	$ChatID = $FilejSon["message"]["chat"]["id"]; // get the User ID, this is unique
+	//$Message = $FilejSon["message"]["text"]; // Get the message sent from user
+    $messageId = $FilejSon["message"]["message_id"]; // get the User ID, this is unique
+	$Message = isset($update['message']["text"]); // Get the message sent from user
+	switch ($Message)
+	{
+		case '/start':
+			$msg = "Welcome $FirstName! I'm a Tutorial Bot.";
+			showKeyboard($ChatID, $msg);
+			break;
+
+		case '/keyboard': // Command to show normal Keyboard
+			$msg = "This is a Tutorial, this Keyboard has 3 buttons, click one to test.";
+			showKeyboard($ChatID, $msg);
+			break;
 
 		case "chatid":
 			$msg = $ChatID;
@@ -49,7 +73,7 @@
 			break;
 
 		default:
-			$msg = "Unknown Command!";
+			$msg = "Comando Sconosciuto!";
 			sendMessage($ChatID, $msg);
 			break;
 	} 
