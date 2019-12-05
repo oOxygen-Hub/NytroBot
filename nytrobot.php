736c9412f1d9ccb7adf52882881df0c07ce1e66b
oOxygen-Hub <58358660+oOxygen-Hub@users.noreply.github.com>
2019-12-05 14:46:46 +0100

    Update nytrobot.php

--- nytrobot.php
+++ nytrobot.php
@@ -8,28 +8,6 @@
 
 	$FirstName = $FilejSon["message"]["chat"]["first_name"]; // Get the name that user set
 	$ChatID = $FilejSon["message"]["chat"]["id"]; // get the User ID, this is unique
-	$Message = $FilejSon["message"]["text"]; // Get the message sent from user
-    $messageId = $FilejSon["message"]["message_id"]; // get the User ID, this is unique
-	
-	switch ($Message)
-	{
-		case '/start':
-			$msg = "Welcome $FirstName! I'm a Tutorial Bot.";
-			showKeyboard($ChatID, $msg);
-			break;
-
-		case '/keyboard': // Command to show normal Keyboard
-			$msg = "This is a Tutorial, this Keyboard has 3 buttons, click one to test.";
-<?php
-	
-	$botToken = "1036235226:AAEHiMaeIW0CRa34AvVG7H4JWuUBJn_poIM"; // Api TOKEN to our bot
-	$website = "https://api.telegram.org/bot".$botToken;
-
-	$FilejSon = file_get_contents("php://input"); // Take the url input, in this case will be executed method getUpdates that return Update.
-	$FilejSon = json_decode($FilejSon, TRUE); // Decode the variable before because now we can search with key (because it's a dictionary)
-
-	$FirstName = $FilejSon["message"]["chat"]["first_name"]; // Get the name that user set
-	$ChatID = $FilejSon["message"]["chat"]["id"]; // get the User ID, this is unique
 	//$Message = $FilejSon["message"]["text"]; // Get the message sent from user
     $messageId = $FilejSon["message"]["message_id"]; // get the User ID, this is unique
 	$Message = isset($update['message']["text"]); // Get the message sent from user
@@ -57,8 +35,9 @@
 			
 		case "Hide Keyboard": // This is the same text inside a Keyboard
 		    //$msg = "Welcome $messageId! I'm a Tutorial Bot.";
-			 $msg = "<a href='https://www.carspecs.us/photos/c8447c97e355f462368178b3518367824a757327-2000.jpg'> ‏ </a>";
-			sendMessage($ChatID, $msg);
+				    $message_body = "<b>Questi sono i tuoi dettagli</b> \n $nome \n $chatId \n $queryusername \n <a href='https://www.carspecs.us/photos/c8447c97e355f462368178b3518367824a757327-2000.jpg'></a>"; 
+			 $msg = "ciaooo";
+			sendMessage($ChatID, $message_body);
 			break;
 
 		case "Inline Keyboard": // This is the same text inside a Keyboard
@@ -80,36 +59,6 @@
 
 	function sendMessage($chat_id, $text)
 	{
-		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text);
-		file_get_contents($url);
-	}
-
-	function showKeyboard($chat_id, $text)
-	{
-		$jSonCodeKeyboard = '&reply_markup={"keyboard":[["Normal%20Keyboard"],["Hide%20Keyboard","Remove%20Keyboard"]],"resize_keyboard":true}';
-		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
-		file_get_contents($url);
-	}
-
-	function removeKeyboard($chat_id, $text)
-	{
-		$jSonCodeKeyboard = '&reply_markup={"remove_keyboard":true}';
-		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
-		file_get_contents($url);
-	}
-
-	function inlineKeyboard($chat_id, $text) // This is an useless type of this keyboard, in a specific Tutorial I show an useful usage of this keyboard.
-	{
-		$jSonCodeKeyboard = '&reply_markup={"inline_keyboard":[[{"text":"API%20Bot%20Telegram","url":"https://core.telegram.org/bots/api"},{"text":"Google","url":"https://www.google.com"}]]}';
-		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
-		file_get_contents($url);
-	}
-	
-	function deleteMessage($chat_id, $querymsgid) // This is an useless type of this keyboard, in a specific Tutorial I show an useful usage of this keyboard.
-	{
-	$url = $GLOBALS[website]."/deleteMessage?chat_id=$ChatID&message_id=$querymsgid";
-    file_get_contents($url);
-	}
 		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text);
 		file_get_contents($url);
 	}
